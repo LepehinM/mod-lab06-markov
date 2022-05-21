b724cd736fa62b761972469aeb49d64968dd7519
Makar <anl-pr@yandex.ru>
2022-05-21 19:45:54 +0300

    comment

--- include/textgen.h
+++ include/textgen.h
@@ -1,24 +1,19 @@
-// Copyright 2022 UNN-IASR
-#pragma once
-#include <stdio.h>
 #include <string>
-#include <iostream>
-#include <vector>
 #include <deque>
+#include <vector>
 #include <map>
-#include <sstream>
-#include <fstream>
-#include <random>
-
-class Gen {
- private:
-    std::string data;
-    std::vector<std::string> words;
-    typedef deque<std::string> prefix;
-    map<prefix, vector<std::string> > statetab;
-    int NPREF, MAXGEN;
- public:
-    Gen(std::string arr, int val1, int val2);
-    std::string getText();
-    Gen(map<prefix, vector<std::string> >, vector<std::string>, int, int);
+using namespace std;
+class Gen
+{
+private:
+    string data;
+    vector<string> words;
+    typedef deque<string> prefix;
+    map<prefix, vector<string> > statetab;
+    int NPREF;
+    int MAXGEN;
+public:
+    Gen(string arr, int val1, int val2);
+    string getText();
+    Gen(map<prefix, vector<string> > val, vector<string> words, int val1, int val2);
 };
