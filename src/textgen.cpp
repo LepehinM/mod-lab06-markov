ee250bf2aa971f73bff6cbf6728627d5cc379c7a
Makar <anl-pr@yandex.ru>
2022-05-21 13:48:36 +0300

    comment

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -13,9 +13,10 @@ Gen::Gen(string arr, int val1, int val2) {
             count++;
             words.push_back("");
             words[count] = words[count] + arr[i];
-        } else if (state == 1 && (arr[i] == ' ' || arr[i] == '\r' || arr[i] == '\n')) {
-            state = 0; }
-        else if (state == 1 && arr[i] != ' ' && arr[i] != '\r' && arr[i] != '\n') {
+} else if (state == 1 && (arr[i] == ' ' || arr[i] == '\r' || arr[i] == '\n')) {
+            state = 0;
+} else if (state == 1 && arr[i] != ' ' && arr[i] != '\r' && arr[i] != '\n') {
+else if (state == 1 && arr[i] != ' ' && arr[i] != '\r' && arr[i] != '\n') {
             words[count] = words[count] + arr[i]; }
         i++;
     }
@@ -46,8 +47,12 @@ string Gen::getText() {
         if (statetab[str][statetab[str].size() - 1] == "END_OF_FILE") {
         val = rand_r() % (statetab[str].size() - 1); }
         answer = answer + statetab[str][val] + ' ';
-        if (count * 100 - answer.size() < 0 || count * 100 - answer.size() > 100) {
+        if (count * 100 - answer.size() < 0 ) {
             count++;
+            answer = answer + '\n';
+        }
+		if (count * 100 - answer.size() > 100) {
+			count++;
             answer = answer + '\n';
         }
         string tmp = statetab[str][val];
